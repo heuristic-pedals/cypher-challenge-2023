ce76adf7bee07d6e8e07a99424622057f0e1a832
Heuristic Pedals <heuristicpedals@gmail.com>
2023-12-28 20:50:38 +0000

    chore: initial caesar struct setup

--- src/challenges/chapter01.rs
+++ src/challenges/chapter01.rs
@@ -3,5 +3,9 @@ use std::path::Path;
 
 pub fn part_a(input_path: &Path) {
     println!("Running C01, Part A, using {:?}", input_path);
-    caesar::print_hello_world();
+    let mut shift = caesar::Caesar::init(input_path);
+    dbg!(&shift.key);
+    let decoded = shift.decode();
+    dbg!(&shift.key);
+    println!("Decoded input: {}", decoded);
 }
--- src/technique/substitution/caesar.rs
+++ src/technique/substitution/caesar.rs
@@ -1,3 +1,23 @@
-pub fn print_hello_world() -> () {
-    println!("Hello, World!");
+use std::path::Path;
+use std::fs::read_to_string;
+
+#[derive(Debug)]
+pub struct Caesar {
+    _coded_input: String,
+    pub key: Option<u8>,
+}
+
+impl Caesar {
+    pub fn init(input_path: &Path) -> Caesar {
+        let input = read_to_string(input_path).unwrap();
+        Caesar {
+            _coded_input: input,
+            key: None,
+        }
+    }
+    pub fn decode(&mut self) -> String {
+        println!("Decoding a caesar cypher...");
+        self.key = Some(10);
+        String::from("test")
+    }
 }
