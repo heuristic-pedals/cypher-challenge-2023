fa2523af03146aed104b4e96d092a00c1bd648eb
Heuristic Pedals <heuristicpedals@gmail.com>
2023-12-30 17:04:09 +0000

    refactor: added most common uppercase ascii fn

--- src/technique/substitution/caesar.rs
+++ src/technique/substitution/caesar.rs
@@ -15,18 +15,14 @@ impl Caesar {
         Ok(Caesar { _coded_input: input,key: None })
     }
     pub fn decode(&mut self) -> String {
-        let input_bytes = self._coded_input.as_bytes()
-            .iter()
-            .filter(|b| **b >= 65 as u8 && **b <= 90)
-            .collect::<Vec<&u8>>();
-
-        let mut freqs: HashMap<u8, usize> = HashMap::new();
-        for b in input_bytes {
-            let count = freqs.entry(*b).or_insert(0);
-            *count += 1;
-        }
-        let most_common_bytes = freqs.iter().max_by_key(|b| b.1).unwrap().0;
-        let key: i16 = 69 - *most_common_bytes as i16;
+        // a common iterator - variable to only do this once
+        let input_bytes_iter: std::slice::Iter<'_, u8> = self._coded_input
+            .as_bytes()
+            .iter();
+        
+        // 69 is the E char - calculate shift needed to map most common onto this
+        let most_common_bytes = Caesar::most_common_uppercase_ascii(&input_bytes_iter);
+        let key: i16 = 69 - most_common_bytes as i16;
         self.key = Some(key);
 
         let decoded_input = self._coded_input.as_bytes()
@@ -37,6 +33,18 @@ impl Caesar {
                 .collect::<Vec<char>>();
 
         String::from_iter(decoded_input)
+    }
+    fn most_common_uppercase_ascii(bytes_iter: &std::slice::Iter<'_, u8>) -> u8 {
+        let input_bytes_chars = bytes_iter.clone()
+            .filter(|x| x.is_ascii_uppercase())
+            .collect::<Vec<&u8>>();
+
+        let mut freqs: HashMap<u8, usize> = HashMap::new();
+        for b in input_bytes_chars {
+            let count = freqs.entry(*b).or_insert(0);
+            *count += 1;
+        }
+        freqs.iter().max_by_key(|b| b.1).unwrap().0.clone()
     }
     pub fn key(& self) -> Option<i16> {
         self.key
