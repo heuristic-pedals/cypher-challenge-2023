3b120ba14e5405128e18e3d12fbfb2013fcaaeb6
Heuristic Pedals <heuristicpedals@gmail.com>
2023-12-30 17:23:48 +0000

    refactor: separate decoding map fn; cargo fmt

--- src/challenges/chapter01.rs
+++ src/challenges/chapter01.rs
@@ -5,7 +5,7 @@ pub fn part_a(input_path: &Path) {
     println!("Running C01, Part A, using {:?}", input_path);
     let mut shift = match caesar::Caesar::init(input_path) {
         Ok(initialised) => initialised,
-        Err(error) => panic!("Unable to initialise Caesar decoder {}", error)
+        Err(error) => panic!("Unable to initialise Caesar decoder {}", error),
     };
 
     let decoded = shift.decode();
--- src/technique/substitution/caesar.rs
+++ src/technique/substitution/caesar.rs
@@ -1,7 +1,7 @@
+use std::collections::HashMap;
+use std::fs::read_to_string;
 use std::io;
 use std::path::Path;
-use std::fs::read_to_string;
-use std::collections::HashMap;
 
 #[derive(Debug)]
 pub struct Caesar {
@@ -12,30 +12,30 @@ pub struct Caesar {
 impl Caesar {
     pub fn init(input_path: &Path) -> Result<Caesar, io::Error> {
         let input = read_to_string(input_path)?;
-        Ok(Caesar { _coded_input: input,key: None })
+        Ok(Caesar {
+            _coded_input: input,
+            key: None,
+        })
     }
+
     pub fn decode(&mut self) -> String {
-        // a common iterator - variable to only do this once
-        let input_bytes_iter: std::slice::Iter<'_, u8> = self._coded_input
-            .as_bytes()
-            .iter();
-        
+        // a common iterator - set as variable to only create this once
+        let input_bytes_iter: std::slice::Iter<'_, u8> = self._coded_input.as_bytes().iter();
+
         // 69 is the E char - calculate shift needed to map most common onto this
         let most_common_bytes = Caesar::most_common_uppercase_ascii(&input_bytes_iter);
         let key: i16 = 69 - most_common_bytes as i16;
         self.key = Some(key);
-
-        let decoded_input = self._coded_input.as_bytes()
-                .iter()
-                .filter(|x| x.is_ascii())
-                .map(|b| if *b >= 65 && *b <= 90 {((*b as i16 - 65 + key) % 26 + 26) % 26 + 65} else { *b as i16 })
-                .map(|i| i as u8 as char)
-                .collect::<Vec<char>>();
+        let decoded_input = input_bytes_iter
+            .map(|x| Caesar::map_char(*x, key))
+            .collect::<Vec<char>>();
 
         String::from_iter(decoded_input)
     }
+
     fn most_common_uppercase_ascii(bytes_iter: &std::slice::Iter<'_, u8>) -> u8 {
-        let input_bytes_chars = bytes_iter.clone()
+        let input_bytes_chars = bytes_iter
+            .clone()
             .filter(|x| x.is_ascii_uppercase())
             .collect::<Vec<&u8>>();
 
@@ -46,7 +46,14 @@ impl Caesar {
         }
         freqs.iter().max_by_key(|b| b.1).unwrap().0.clone()
     }
-    pub fn key(& self) -> Option<i16> {
+
+    fn map_char(coded_char: u8, key: i16) -> char {
+        if !coded_char.is_ascii_uppercase() {
+            return coded_char as char;
+        }
+        (((coded_char as i16 - 65 + key) % 26 + 26) % 26 + 65) as u8 as char
+    }
+    pub fn key(&self) -> Option<i16> {
         self.key
     }
 }
