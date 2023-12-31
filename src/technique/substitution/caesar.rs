e21c5f34d0cba4d49414b9a8444aedc174cafbf6
Heuristic Pedals <heuristicpedals@gmail.com>
2023-12-31 17:09:55 +0000

    fix: decode with utf-8 encoding

--- src/technique/substitution/caesar.rs
+++ src/technique/substitution/caesar.rs
@@ -2,6 +2,7 @@ use std::collections::HashMap;
 use std::fs::{read_to_string, self};
 use std::io;
 use std::path::Path;
+use std::str::Chars;
 
 #[derive(Debug)]
 pub struct Caesar {
@@ -22,14 +23,14 @@ impl Caesar {
 
     pub fn decode(&mut self) -> String {
         // a common iterator - set as variable to only create this once
-        let input_bytes_iter: std::slice::Iter<'_, u8> = self._coded_input.as_bytes().iter();
+        let input_chars: Chars<'_> = self._coded_input.chars();
 
         // 69 is the E char - calculate shift needed to map most common onto this
-        let most_common_bytes = Caesar::most_common_uppercase_ascii(&input_bytes_iter);
+        let most_common_bytes = Caesar::most_common_uppercase_ascii(&input_chars);
         let key: i16 = 69 - most_common_bytes as i16;
         self.key = Some(key);
-        let decoded_input = input_bytes_iter
-            .map(|x| Caesar::map_char(*x, key))
+        let decoded_input = input_chars
+            .map(|x| Caesar::map_char(x, key))
             .collect::<Vec<char>>();
 
         let decoded_input = String::from_iter(decoded_input);
@@ -37,25 +38,25 @@ impl Caesar {
         decoded_input
     }
 
-    fn most_common_uppercase_ascii(bytes_iter: &std::slice::Iter<'_, u8>) -> u8 {
-        let input_bytes_chars = bytes_iter
+    fn most_common_uppercase_ascii(input_chars: &Chars<'_>) -> u8 {
+        let input_bytes_chars = input_chars
             .clone()
             .filter(|x| x.is_ascii_uppercase())
-            .collect::<Vec<&u8>>();
+            .collect::<Vec<char>>();
 
-        let mut freqs: HashMap<u8, usize> = HashMap::new();
+        let mut freqs: HashMap<char, usize> = HashMap::new();
         for b in input_bytes_chars {
-            let count = freqs.entry(*b).or_insert(0);
+            let count = freqs.entry(b).or_insert(0);
             *count += 1;
         }
-        freqs.iter().max_by_key(|b| b.1).unwrap().0.clone()
+        freqs.iter().max_by_key(|b| b.1).unwrap().0.clone() as u8
     }
 
-    fn map_char(coded_char: u8, key: i16) -> char {
-        if !coded_char.is_ascii_uppercase() {
-            return coded_char as char;
+    fn map_char(coded: char, key: i16) -> char {
+        if !coded.is_ascii_uppercase() {
+            return coded as char;
         }
-        (((coded_char as i16 - 65 + key) % 26 + 26) % 26 + 65) as u8 as char
+        (((coded as i16 - 65 + key) % 26 + 26) % 26 + 65) as u8 as char
     }
 
     pub fn key(&self) -> Option<i16> {
@@ -63,7 +64,6 @@ impl Caesar {
     }
 
     pub fn write_decoded_input(& self, output_path: &Path) -> Result<(), io::Error> {
-
         match &self._decoded_input {
             Some(decoded_input) => fs::write(output_path, decoded_input),
             None => {
